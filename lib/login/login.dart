15a7b95f3ad23352e2f3217b09802d736cdffd29
estarossa10 <dev.kwa21@gmail.com>
2020-11-28 22:28:34 +0700

    Edit Commit

--- lib/login/login.dart
+++ lib/login/login.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class LoginPage extends StatefulWidget{
   @override
@@ -7,91 +8,166 @@ class LoginPage extends StatefulWidget{
 }
 
 class _SearchFormState extends State<LoginPage> {
+
+  String _email, _password;
+  final GlobalKey<FormState> _formkey = GlobalKey<FormState>();
+
   @override
   Widget build(context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
       backgroundColor: Colors.white,
       body: Container(
-        child: Column(
-          children: [
-            Container(
-              height: 300,
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage("assets/images/background.png"),
-                  // fit: BoxFit.fill
-                )
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                width: size.width,
+                height: size.height * 0.35,
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage("assets/images/background.png"),
+                      // fit: BoxFit.fill
+                    )
+                ),
               ),
-            ),
-            Padding(
-              padding: EdgeInsets.all(30),
-              child: Column(
-                children: [
-                  Container(
-                    padding: EdgeInsets.all(5),
-                    decoration: BoxDecoration(
-                      color: Colors.red,
-                      borderRadius: BorderRadius.circular(0.5),
-                    ),
-                    child: Column(
-                      children: [
-                        Container(
-                          padding: EdgeInsets.all(8),
-                          decoration: BoxDecoration(
-                            border: Border(
-                              bottom: BorderSide(color: Colors.red)
-                            )
-                          ),
-                          child: TextField(
-                            decoration: InputDecoration(
-                              border: InputBorder.none,
-                              hintText: 'Email',
-                              hintStyle: TextStyle(color: Colors.grey)
-                            ),
-                          ),
+              Padding(
+                padding: EdgeInsets.all(30),
+                child: Column(
+                  children: [
+                    Form(
+                      key: _formkey,
+                      child: Container(
+                        width: size.width * 0.8,
+                        padding: EdgeInsets.all(5),
+                        decoration: BoxDecoration(
+                          color: Colors.white,
                         ),
-                        Container(
-                          padding: EdgeInsets.all(8),
-                          decoration: BoxDecoration(
-                              border: Border(
-                                  bottom: BorderSide(color: Colors.red)
-                              )
-                          ),
-                          child: TextField(
-                            decoration: InputDecoration(
-                                border: InputBorder.none,
-                                hintText: 'Password',
-                                hintStyle: TextStyle(color: Colors.grey)
+                        child: Column(
+                          children: [
+                            Container(
+                              padding: EdgeInsets.all(8),
+                              decoration: BoxDecoration(
+                                  border: Border(
+                                      bottom: BorderSide(color: Colors.red)
+                                  )
+                              ),
+                              child: TextFormField(
+                                onSaved: (input) {_email = input;},
+                                decoration: InputDecoration(
+                                    border: InputBorder.none,
+                                    hintText: 'Email',
+                                    hintStyle: TextStyle(color: Colors.grey)
+                                ),
+                                validator: (input) {
+                                  if(input.isEmpty) return 'Please provide an email';
+                                  // if(!input.contains(RegExp(r'@'))) return 'Please provide an email';
+                                  return null;
+                                },
+                              ),
+                            ),
+                            Container(
+                              padding: EdgeInsets.all(8),
+                              decoration: BoxDecoration(
+                                  border: Border(
+                                      bottom: BorderSide(color: Colors.red)
+                                  )
+                              ),
+                              child: TextFormField(
+                                onSaved: (input) {_password = input;},
+                                decoration: InputDecoration(
+                                    border: InputBorder.none,
+                                    hintText: 'Password',
+                                    hintStyle: TextStyle(color: Colors.grey)
+                                ),
+                                obscureText: true,
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(5),
+                child: Column(
+                  children: [
+                    Container(
+                      width: size.width,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                      ),
+                      child: Column(
+                        children: [
+                          Container(
+                            width: size.width * 0.8,
+                            child: RawMaterialButton(
+                              onPressed: () {logIn();},
+                              fillColor:  Colors.red,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              child: Padding(
+                                padding: const EdgeInsets.all(15),
+                                child: Text(
+                                  'LOGIN',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 18,
+                                  ),
+                                ),
+                              ),
                             ),
                           ),
-                        )
-                      ],
-                    ),
-                  )
-                ],
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.all(30),
-              child: Column(
-                children: [
-                  Container(
-                    decoration: BoxDecoration(
-                      color: Colors.blue,
-                      borderRadius: BorderRadius.circular(0.5),
-                    ),
-                    child: Column(
-                      children: [
-
-                      ],
-                    ),
-                  )
-                ],
-              ),
-            )
-          ],
+                          Container(
+                            padding: EdgeInsets.symmetric(vertical: 25),
+                            width: size.width * 0.8,
+                            child: RawMaterialButton(
+                              onPressed: () {/*TODO go to register page*/ },
+                              fillColor:  Colors.red,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              child: Padding(
+                                padding: const EdgeInsets.all(15),
+                                child: Text(
+                                  'REGISTER',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 18,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              )
+            ],
+          )
         ),
       ),
     );
   }
+
+  Future<void> logIn() async {
+    final formState = _formkey.currentState;
+
+    if(formState.validate()){
+      formState.save();
+      try{
+        User user = (await FirebaseAuth.instance.signInWithEmailAndPassword(email: _email, password: _password)) as User;
+        print(user.email);
+        //TODO go to search page
+      }catch(e){
+        print(e.message);
+      }
+    }
+  }
+
 }
