d22fd6f8451fdbec9f5e0081db1ab0869c8e1298
estarossa10 <dev.kwa21@gmail.com>
2020-12-07 20:43:30 +0700

    Edit Commit

---
+++ lib/loading/loading_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class loadingScreen extends StatefulWidget {
+  @override
+  _loadingScreenState createState() => _loadingScreenState();
+}
+
+class _loadingScreenState extends State<loadingScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:ratatouille/login/login.dart';
+import 'package:firebase_core/firebase_core.dart';
 // import 'package:ratatouille/search_page/search.dart';
 
 void main() async {
@@ -11,16 +12,36 @@ void main() async {
 
 class Ratatouille extends StatelessWidget {
   // This widget is the root of your application.
+
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Ratatouille',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: LoginPage(),
-      debugShowCheckedModeBanner: false,
+    return FutureBuilder(
+      // Initialize FlutterFire:
+      future: _initialization,
+      builder: (context, snapshot) {
+        // Check for errors
+        if (snapshot.hasError) {
+          print('error happened');
+        }
+
+        // Once complete, show your application
+        if (snapshot.connectionState == ConnectionState.done) {
+          return MaterialApp(
+            title: 'Ratatouille',
+            theme: ThemeData(
+              primarySwatch: Colors.blue,
+              visualDensity: VisualDensity.adaptivePlatformDensity,
+            ),
+            home: LoginPage(),
+            debugShowCheckedModeBanner: false,
+          );;
+        }
+
+        // Otherwise, show something whilst waiting for initialization to complete
+        print('null');
+      },
     );
   }
 }
